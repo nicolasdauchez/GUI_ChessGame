7244d1f10ba3063e210b9ca694c9d1750e8abbf6
Lumy <lumy@lumy.me>
2013-12-04 02:57:53 +0000

    Switching for Pawn function also containing catch_event which is maintest for now.

--- src/Chess/ChessGame.java
+++ src/Chess/ChessGame.java
@@ -4,7 +4,6 @@
 package Chess;
 import java.util.List;
 import java.util.ArrayList;
-
 import org.apache.commons.lang3.ArrayUtils;
 /**
  * @author Lumy-
@@ -23,7 +22,6 @@ public class ChessGame {
 		addLinePion(eColor.White);
 		addHeadLine(eColor.Black);
 		addHeadLine(eColor.White);
-		elem.print();
 	}
 	
 	/**
@@ -37,8 +35,8 @@ public class ChessGame {
 		catchEvent(null, null);
 	}
 
-	private void add(eColor c, Position p, eClass t) {
-		elem.add(new Pion(c, p, t));
+	private void add(eColor c, Position p, ePawns t) {
+		elem.add(new Pawn(c, p, t));
 	}
 
 	private void addLinePion(eColor c) {
@@ -48,8 +46,7 @@ public class ChessGame {
 			p.row = 2;
 		p.column = 'a';
 		while (p.column != 'i') {
-			
-			add(c, new Position(p), eClass.Pion);
+			add(c, new Position(p), ePawns.Pawn);
 			p.column += 1;
 		}
 	}
@@ -60,7 +57,7 @@ public class ChessGame {
 		if (c == eColor.White)
 			p.row = 1;
 		p.column = 'a';
-		eClass e[] = { eClass.Tower, eClass.Cavalery, eClass.Crazy, eClass.King, eClass.Queen, eClass.Crazy, eClass.Cavalery, eClass.Tower };
+		ePawns e[] = { ePawns.Tower, ePawns.Cavalery, ePawns.Crazy, ePawns.King, ePawns.Queen, ePawns.Crazy, ePawns.Cavalery, ePawns.Tower };
 		if (c == eColor.White)
 			ArrayUtils.reverse(e);
 		for (int i = 0; i < e.length; i++) {
@@ -76,25 +73,191 @@ public class ChessGame {
 		 */
 		
 		//
-		Pion p = elem.get(1);
+		Pawn p = elem.get(1);
 		Position t = new Position(p.GetPosition());
 		p.print();
 		while (t.row != 2)
 		{
 			t.row -= 1;
-		    Rules.DoMovePion(p, t, elem);
+		  Rules.DoMovePawns(p, t, elem);
 		}
-	    elem.print();
+		 t.row = 4;
+		 t.column = 'b';
+		 if (Rules.DoMovePawns(p, t, elem))
+		   	System.out.println("bad");
+		 else
+		   	System.out.println("cool");
+	    p.print();
 	    t.row = 2;
 	    t.column = 'a';
+
 	    p = elem.get(elem.indexOf(t));
 	    t.row = 3;
 	    t.column = 'b';
-	    if (Rules.DoMovePion(p, t, elem))
+	    if (Rules.DoMovePawns(p, t, elem))
 	    	System.out.println("Cool");
 	    else
 	    	System.out.println("Bad");
 	    elem.print();
+	    t.row = 1;
+	    t.column = 'a';
+	    p = elem.get(elem.indexOf(t));
+	    t.row = 5;
+	    if (Rules.DoMovePawns(p, t, elem))
+	    	System.out.println("Cool");
+	    else
+	    	System.out.println("Bad");
+	    elem.print();
+	    p.print();
+	    t.row = 5;
+	    t.column = 'b';
+	    if (Rules.DoMovePawns(p, t, elem))
+	    	System.out.println("Cool");
+	    else
+	    	System.out.println("Bad");
+	    p.print();
+	    t.row = 6;
+	    t.column = 'c';
+	    if (Rules.DoMovePawns(p, t, elem))
+	    	System.out.println("Bad");
+	    else
+	    	System.out.println("Cool");
+	    p.print();
+	    t.row = 7;
+	    t.column = 'b';
+	    Rules.DoMovePawns(p, t, elem);
+	    t.column = 'a';
+	    System.out.println(elem.size());
+	    if (Rules.DoMovePawns(p, t, elem))
+	    	System.out.println("Cool");
+	    else
+	    	System.out.println("Bad");
+	    System.out.println(elem.size());
+	    elem.print();
+	    t.row = 2;
+	    t.column = 'e';
+	    p = elem.get(elem.indexOf(t));
+	    t.row = 4;
+	    if (Rules.DoMovePawns(p, t, elem))
+	    	System.out.println("Cool");
+	    else
+	    	System.out.println("Bad");
+	    t.row = 6;
+	    if (!Rules.DoMovePawns(p, t, elem))
+	    	System.out.println("Cool");
+	    else
+	    	System.out.println("Bad");
+	    t.row = 5;
+	    Rules.DoMovePawns(p, t, elem);
+	    t.row = 1;
+	    t.column = 'e';
+	    p = elem.get(elem.indexOf(t));
+	    t.row = 2;
+	    t.column = 'e';
+	    if (Rules.DoMovePawns(p, t, elem))
+	    	System.out.println("Cool");
+	    else
+	    	System.out.println("Bad");	    
+	    t.column = 'd';
+	    t.row = 3;
+	    if (Rules.DoMovePawns(p, t, elem))
+	    	System.out.println("Cool");
+	    else
+	    	System.out.println("Bad");	    
+	    t.row = 5;
+	    if (!Rules.DoMovePawns(p, t, elem))
+	    	System.out.println("Cool");
+	    else
+	    	System.out.println("Bad");	    
+	    t.row = 4;
+	    Rules.DoMovePawns(p, t, elem);
+	    t.row = 5;
+	    Rules.DoMovePawns(p, t, elem);
+	    t.row = 6;
+	    Rules.DoMovePawns(p, t, elem);
+	    t.row = 7;
+	    if (Rules.DoMovePawns(p, t, elem))
+	    	System.out.println("Cool");
+	    else
+	    	System.out.println("Bad");
+	    System.out.println(elem.size());
+	    elem.print();
+	    t.row = 8;
+	    t.column = 'c';
+	    p = elem.get(elem.indexOf(t));
+	    t.row = 6;
+	    t.column = 'a';
+	    p.print();
+	    if (Rules.DoMovePawns(p, t, elem))
+	    	System.out.println("Cool");
+	    else
+	    	System.out.println("Bad");
+	    p.print();
+	    t.row = 4;
+	    t.column = 'c';
+	    if (Rules.DoMovePawns(p, t, elem))
+		    p.print();
+	    t.row = 7;
+	    t.column = 'f';
+	    if (Rules.DoMovePawns(p, t, elem))
+		    System.out.println("Bad!!");
+	    elem.print();
+	    t.row = 1;
+	    t.column = 'f';
+	    if (!Rules.DoMovePawns(p, t, elem))
+		    System.out.println("Bad!!");
+	    else
+	    	System.out.println("Cool :)");
+	    elem.print();
+	    t.row = 8;
+	    t.column = 'b';
+	    p = elem.get(elem.indexOf(t));
+	    t.row = 6;
+	    t.column = 'c';
+	    if (!Rules.DoMovePawns(p, t, elem))
+		    System.out.println("Bad!!");
+	    else
+	    	System.out.println("Cool :)");
+	    t.row = 4;
+	    t.column = 'd';
+	    if (!Rules.DoMovePawns(p, t, elem))
+		    System.out.println("Bad!!");
+	    t.row = 2;
+	    t.column = 'c';
+	    if (!Rules.DoMovePawns(p, t, elem))
+	    	System.out.println("Bad!!");
+	    else
+	    	System.out.println("Cool Before!!");
+	    elem.print();
+	    
+	    t.row = 1;
+	    t.column = 'd';
+	    p = elem.get(elem.indexOf(t));
+	    t.row = 2;
+	    t.column = 'c';
+	    if (!Rules.DoMovePawns(p, t, elem))
+		    System.out.println("Bad!!");
+	    else
+	    	System.out.println("Cool Eat :)");
+	    t.row = 4;
+	    t.column = 'a';
+	    if (!Rules.DoMovePawns(p, t, elem))
+		    System.out.println("Bad!!");
+	    else
+	    	System.out.println("Cool :)");
+	    t.row = 4;
+	    t.column = 'h';
+	    if (!Rules.DoMovePawns(p, t, elem))
+		    System.out.println("Bad!!");
+	    else
+	    	System.out.println("Cool :)");
+	    t.row = 6;
+	    t.column = 'h';
+	    if (!Rules.DoMovePawns(p, t, elem))
+		    System.out.println("Bad!!");
+	    else
+	    	System.out.println("Cool :)");
+	    System.out.println(elem.size());
 	    // On mange et on bouge des ptit pion sans probleme
 	    return false;
 	}
