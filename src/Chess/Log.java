81c8d01b707fc6520bf04b66c26285d5ac2ce4a0
Lumy <lumy@lumy.me>
2013-12-07 19:58:00 +0000

    Log Test pour ParserPGN mais on oublie juste commit pour pas problem

--- src/Chess/Log.java
+++ src/Chess/Log.java
@@ -3,7 +3,13 @@
  */
 package Chess;
 import main.Pair;
+
+import com.codethesis.pgnparse.*;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 /**
  * @author Lumy-
  * Log Import and Export with PGN Norme
@@ -17,13 +23,48 @@ public class Log {
 		log = new ArrayList<Pair<Position, Position>>();
 	}
 	
-	public void newGame()
-	{
+	public void newGame() {
 		log.clear();
 	}
 
 	public void Initialize()
 	{
+		PGNSource test;
+
+		try {
+			test = new PGNSource(new File("wcc.PGN"));
+			List<PGNGame> p = test.listGames();
+			
+		} catch (IOException | NullPointerException | PGNParseException | MalformedMoveException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+		
+		//
+		try {
+			test = new PGNSource(new File("wccc1974.PGN"));
+			//System.out.println(test.toString());
+			try {
+				List<PGNGame> p = test.listGames();
+				System.out.println(p.size());
+//				System.out.println(p.toString());
+				for (PGNGame g : p)
+				{
+					int i = 0;
+					for (int max = g.getMovesCount(); i < max; i++) {
+						PGNMove m = g.getMove(i);
+					}
+					break;
+				}
+			} catch (NullPointerException | PGNParseException | IOException
+					| MalformedMoveException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		
 	}
 
