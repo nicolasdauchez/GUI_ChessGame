f7bf778689075a3938e52a45dc4eaf2e8ce274d5
Lumy <lumy@lumy.me>
2013-12-07 15:30:10 +0000

    Modification on Abstract Functor Should move is now no more abstract (still has to code it) Pawn: Rewrite becaise didn't check if the pawn goes diagonal. Tower: add an else to return SameColorCaseOCCUPIED instead of FAIL_UNAUTH... King: Same Than Tower Crazy: Adding some commentary and same than Tower and remove ShouldMove(no more abstract) Cavalery: Rewrwite complete. Is working well no. with commentary Queen: Same Than tower Remove useless import and function isEatSomethingTower(old idea) Rerwrite Loop DoMovePawns to return a Pair<eMoveState, eGameState> eGameState and eMoveState is essentialy for intern ChessGame, but GUI could use at the return to update the data or could call some new function in ChessGame eachtime to update Data.

--- src/Chess/Rules.java
+++ src/Chess/Rules.java
@@ -3,11 +3,9 @@
  */
 package Chess;
 
-import java.util.Iterator;
-import java.util.List;
 import java.util.Hashtable;
 import java.util.Map;
-import java.util.Collections;
+import main.Pair;
 /**
  * @author Lumy-
  *
@@ -28,8 +26,12 @@ public class Rules {
 		 *
 		 */
 		static public abstract class Functor {
-			  public abstract boolean CanMove(Pawn p, Position newPos, BoardGame elem);
-			  public abstract boolean ShouldMove(Pawn p, Position newPos, BoardGame elem);
+			  public abstract eMoveState CanMove(Pawn p, Position newPos, BoardGame elem);
+			  public eGameState ShouldMove(Pawn p, Position newPos, BoardGame elem) {
+				  //List<Pawn> tmp = elem.getNewCopie(p, newPos);
+				  return eGameState.NEXT;
+			}
+			  
 			  public  void execute(Pawn p, Position newPos, BoardGame elem) {
 					if (elem.getObstacleCase(newPos) == eColor.None)
 						p.SetPosition(newPos);
@@ -46,24 +48,29 @@ public class Rules {
 		 *
 		 */
 		static private class DoMovePawn extends Functor {
-			public boolean CanMove(Pawn p, Position newPos, BoardGame elem) {
+			public eMoveState CanMove(Pawn p, Position newPos, BoardGame elem) {
 				if (p.isNotGoodDirection(newPos))
-					return false; // return badPosition enum
-				if (elem.contains(newPos)) // we can do this because we overload Equal to compare with Position
-					if (newPos.column == p.GetPosition().column ||
-					(newPos.column - 1 != p.GetPosition().column && newPos.column + 1 != p.GetPosition().column))
-						if (elem.getObstacleCase(newPos) != p.GetColor())
-							return false; // return BadEat enum
-				if ( (newPos.diffRow(p.GetPosition()) == 2 &&
-					p.isStartPosition()) || newPos.diffRow(p.GetPosition()) == 1)
-						return true;
-					return false;
-			}
-			@Override
-			public boolean ShouldMove(Pawn p, Position newPos, BoardGame elem) {
-				// TODO Auto-generated method stub
-				return true;
-			}
+					return eMoveState.FAIL_PAWNS_BACKWARD; // return badPosition enum
+				if (newPos.column == p.GetPosition().column) // Forward
+					if (elem.contains(newPos)) // enemie forward
+						return eMoveState.FAIL_PAWNS_EAT_FORWARD;
+					else // No enemy Forward
+						{
+							if ((newPos.diffRow(p.GetPosition()) == 2 && p.isStartPosition()) ||// try to make 2 move
+								 newPos.diffRow(p.GetPosition()) == 1) // try to make one move
+									return eMoveState.SUCCESS;
+							return eMoveState.FAIL_UNAUTHORIZED; //To much Move
+						}
+				if ((newPos.column - 1 == p.GetPosition().column || newPos.column + 1 == p.GetPosition().column) && // Move Diagonal
+				    elem.contains(newPos) && elem.getObstacleCase(newPos) != p.GetColor()){ // Contain Something and not Same Color (Enemy)
+					if (newPos.diffRow(p.GetPosition()) == 1) // try to make one move
+					    return eMoveState.SUCCESS;
+				}
+				else
+					return eMoveState.FAIL_SAME_COLOR_CASE_OCCUPIED;
+				return eMoveState.FAIL_UNAUTHORIZED;
+			 }
+			 
 		}
 		/**
 		 * Execute Move For A Tower Pawn Class.
@@ -72,20 +79,16 @@ public class Rules {
 		 */
 		static private class DoMoveTower extends Functor {
 			@Override
-			public boolean CanMove(Pawn p, Position newPos, BoardGame elem) {
+			public eMoveState CanMove(Pawn p, Position newPos, BoardGame elem) {
 				Position oldp = p.GetPosition();
 				if (oldp.sameRow(newPos) || oldp.sameColumn(newPos))
-					if (eColor.None == elem.getObstacleRange(p, newPos) &&
-					elem.getObstacleCase(newPos) != p.GetColor())
-						return true;
-				return false;
+					if (eColor.None == elem.getObstacleRange(p, newPos))
+						if (elem.getObstacleCase(newPos) != p.GetColor())
+							return eMoveState.SUCCESS;
+						else
+							return eMoveState.FAIL_SAME_COLOR_CASE_OCCUPIED;
+				return eMoveState.FAIL_UNAUTHORIZED;
 			}
-			@Override
-			public boolean ShouldMove(Pawn p, Position newPos, BoardGame elem) {
-				// TODO Auto-generated method stub
-				return true;
-			}
-			
 		}
 		/**
 		 * Execute move for a King Pawn Class.
@@ -94,16 +97,14 @@ public class Rules {
 		 */
 		static private class DoMoveKing extends Functor {
 			@Override
-			public boolean CanMove(Pawn p, Position newPos, BoardGame elem) {
-				if (newPos.diffMultiple(p.GetPosition()) != 1 || elem.getObstacleCase(newPos) == p.GetColor())
-					return false; // A King Can't Move More Than One
-				return true;
+			public eMoveState CanMove(Pawn p, Position newPos, BoardGame elem) {
+				if (newPos.diffMultiple(p.GetPosition()) != 1)
+					return eMoveState.FAIL_UNAUTHORIZED; // A King Can't Move More Than One
+				if (elem.getObstacleCase(newPos) == p.GetColor())
+					return eMoveState.FAIL_SAME_COLOR_CASE_OCCUPIED;
+				return eMoveState.SUCCESS;
 			}
-			@Override
-			public boolean ShouldMove(Pawn p, Position newPos, BoardGame elem) {
-				//check if the newPos don't PutinCheckMat
-				return true;
-			}
+
 		}
 		/**
 		 * Execute move for a Crazy Pawn Class.
@@ -112,18 +113,13 @@ public class Rules {
 		 */
 		static private class DoMoveCrazy extends Functor {
 			@Override
-			public boolean CanMove(Pawn p, Position newPos, BoardGame elem) {
-				if (newPos.diffMultiple(p.GetPosition()) == -1 ||
-					elem.getObstacleRange(p, newPos) != eColor.None)
-					return false; // Can't Move there
+			public eMoveState CanMove(Pawn p, Position newPos, BoardGame elem) {
+				if (newPos.diffMultiple(p.GetPosition()) == -1 || //Check if RealDiagonal
+					elem.getObstacleRange(p, newPos) != eColor.None) // Check if Nothing on the way
+					return eMoveState.FAIL_UNAUTHORIZED; // Can't Move there
 				if (elem.getObstacleCase(newPos) == p.GetColor())
-					return false; // can't go on your pawn
-				return true;
-			}
-			@Override
-			public boolean ShouldMove(Pawn p, Position newPos, BoardGame elem) {
-				//check if the newPos don't PutinCheckMat
-				return true;
+					return eMoveState.FAIL_SAME_COLOR_CASE_OCCUPIED; // can't go on your pawn
+				return eMoveState.SUCCESS;
 			}
 		}
 		/**
@@ -133,19 +129,16 @@ public class Rules {
 		 */
 		static private class DoMoveCavalery extends Functor {
 			@Override
-			public boolean CanMove(Pawn p, Position newPos, BoardGame elem) {
+			public eMoveState CanMove(Pawn p, Position newPos, BoardGame elem) {
 				Position oldp = p.GetPosition();
-				int d1 = oldp.diffRow(newPos);
-				if ((d1 == 1 && oldp.diffColumn(newPos) == 2) ||
-					(d1 == 2 && oldp.diffColumn(newPos) == 1))
-					if (elem.getObstacleCase(newPos) != p.GetColor())
-						return true; // Can't Move there
-				return false; // can't go on your pawn
-			}
-			@Override
-			public boolean ShouldMove(Pawn p, Position newPos, BoardGame elem) {
-				//check if the newPos don't PutinCheckMat
-				return true;
+				int d1 = oldp.diffRow(newPos); //GetDiffRow
+				if ((d1 == 1 && oldp.diffColumn(newPos) == 2) || //CheckPosition
+					(d1 == 2 && oldp.diffColumn(newPos) == 1)) // CheckPosition
+					if (elem.getObstacleCase(newPos) != p.GetColor()) // Not Occupied or Not Same Color
+						return eMoveState.SUCCESS;
+					else
+						return eMoveState.FAIL_SAME_COLOR_CASE_OCCUPIED;
+				return eMoveState.FAIL_UNAUTHORIZED;
 			}
 		}
 		/**
@@ -155,16 +148,13 @@ public class Rules {
 		 */
 		static private class DoMoveQueen extends Functor {
 			@Override
-			public boolean CanMove(Pawn p, Position newPos, BoardGame elem) {
-				if (newPos.diffMultiple(p.GetPosition()) == -1 ||
-						elem.getObstacleRange(p, newPos) != eColor.None)
-					return false; // A King Can't Move More Than One
-				return true;
-			}
-			@Override
-			public boolean ShouldMove(Pawn p, Position newPos, BoardGame elem) {
-				//check if the newPos don't PutinCheckMat
-				return true;
+			public eMoveState CanMove(Pawn p, Position newPos, BoardGame elem) {
+				if (newPos.diffMultiple(p.GetPosition()) == -1 || // Check True Diagonal 
+						elem.getObstacleRange(p, newPos) != eColor.None) // Check no Obstacle
+					return eMoveState.FAIL_CHECK; // Obstacle or no True Diagonal
+				if (elem.getObstacleCase(newPos) == p.GetColor())
+					return eMoveState.FAIL_SAME_COLOR_CASE_OCCUPIED;
+				return eMoveState.SUCCESS;
 			}
 		}
 		static final public Map<ePawns, Functor> MapFunction = createMap();
@@ -176,44 +166,53 @@ public class Rules {
 	        aMap.put(ePawns.Crazy, new DoMoveCrazy());
 	        aMap.put(ePawns.Cavalery, new DoMoveCavalery());
 	        aMap.put(ePawns.Queen, new DoMoveQueen());
-	 	        return aMap;
+	 	    return aMap;
 	    }
 	};
 
-
-	
-	private static boolean isEatSomethingTower(Pawn p, Position newPos,
-			BoardGame elem) {
-		// TODO Auto-generated method stub
-		return false;
+	/**
+	 * Just test the eGameState with the color of currentPlayer
+	 * if Rules.MapFunctor.MapFunction[key].ShouldMove return a CheckMate for current player
+	 * or if error pass and if eGameState == SAME (should not arrive there)
+	 * @param r2
+	 * @param c
+	 * @return
+	 */
+	private static boolean TestState(eGameState r2, eColor c) {
+		if (r2 == eGameState.SAME || (r2 == eGameState.CHECK_MATE_B && c == eColor.Black) ||
+				(r2 == eGameState.CHECK_MATE_W && c == eColor.White))
+			return false;
+		return true;
 	}
 
-	
-
-	
-	
-
-	static public boolean DoMovePawns(Pawn p, Position newPos, BoardGame elem)
+	/**
+	 * Make The move for one Pawn (whatever is the ePawns)
+	 * begin by lookin for the key[ePawns] in Rules.MapFunctor.MapFunction
+	 * then Call: Value[key].canMove()
+	 * 			  Value[key].ShouldMove()
+	 * 			  Value[Key].execute()
+	 * @param p
+	 * @param newPos
+	 * @param elem
+	 * @return  <- if move not valid eGameState=SAME
+	 */
+	static public Pair<eMoveState, eGameState> DoMovePawns(Pawn p, Position newPos, BoardGame elem)
 	{
+		eMoveState r = eMoveState.FAIL_CLASS_UNKNOWN;
+		eGameState r2 = eGameState.SAME;
 		for (Map.Entry<ePawns, MapFunctor.Functor> entry : Rules.MapFunctor.MapFunction.entrySet()) {
 		    if (p.GetClass().equals((ePawns)entry.getKey())) {
-		    	if ( ((MapFunctor.Functor)entry.getValue()).CanMove(p, newPos, elem) )
-			    	if ( ((MapFunctor.Functor)entry.getValue()).ShouldMove(p, newPos, elem) ) {
-			    		((MapFunctor.Functor)entry.getValue()).execute(p, newPos, elem);
-			    		return true;
-			    	}
-		    	return false;
+		    	if ((r = ((MapFunctor.Functor)entry.getValue()).CanMove(p, newPos, elem)) == eMoveState.SUCCESS) {
+		    		r2 = ((MapFunctor.Functor)entry.getValue()).ShouldMove(p, newPos, elem);
+		    		if (TestState(r2, p.GetColor())) {
+		    			((MapFunctor.Functor)entry.getValue()).execute(p, newPos, elem);
+		    			return new Pair<eMoveState, eGameState>(r, r2);
+		    		}
+		    		else
+		    			r2 = eGameState.SAME;
+			    }
 		    }
 		}
-		return false;
-	}
-
-	static public boolean CheckRule(String s, List<Pawn> p)
-	{
-		/*if (Character.isUpperCase(s.charAt(0)) && s.charAt(0) != 'P')
-			return CheckHead(s.substring(1)); //King Queen...
-		else
-			return CheckPion( (s.charAt(0) == 'P') ? s.substring(1) : s);//Pion*/
-		return false;
+  		return new Pair<eMoveState, eGameState>(r, r2);
 	}
 };
