85bfc4c8d110a239a03e39e528d442d2cfc6fb35
Lumy <lumy@lumy.me>
2013-12-17 03:30:49 +0000

    eMoveState Sexy javaDoc
    Signed-off-by: Lumy <lumy@lumy.me>

--- src/Chess/eMoveState.java
+++ src/Chess/eMoveState.java
@@ -1,5 +1,18 @@
 package Chess;
 
+/**
+ * Represent a State Move
+ * {@link #SUCCESS} is if all is good, and the game is keep continuing
+ * {@link #CASTLING} is when the player click for a {@link Rules.Castling}. Need user approval
+ * {@link #FAIL_CHECK} is when the player is in {@link eGameState#CHECK_KING_B} and can't do the mouvement.
+ * {@link #FAIL_CLASS_UNKNOWN} should never happen. the {@link ePawns} of the Pawn is not Known
+ * {@link #FAIL_PAWNS_EAT_FORWARD} is when a {@link ePawns#PAWN} try to eat forward
+ * {@link #FAIL_PAWNS_BACKWARD} is when a {@link ePawns#PAWN} try to go backward
+ * {@link #FAIL_SAME_COLOR_CASE_OCCUPIED} is when the secondClick of Player is occupied by the same {@link eColor}
+ * {@link #FAIL_UNAUTHORIZED} is an when a {@link ePawns} try to go somewhere he can't.
+ * @author Lumy-
+ *
+ */
 public enum eMoveState {
 	SUCCESS,
 	CASTLING,
