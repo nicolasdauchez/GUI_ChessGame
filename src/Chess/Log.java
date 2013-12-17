f87f184b3439ff68c153e2e060fa3150e169db86
Lumy <lumy@lumy.me>
2013-12-17 05:28:37 +0000

    Log.Tree SexyJavadoc Updated
    Signed-off-by: Lumy <lumy@lumy.me>

--- src/Chess/Log.java
+++ src/Chess/Log.java
@@ -23,6 +23,16 @@ import com.thoughtworks.xstream.XStream;
  * Also log each Mouvement. You can move in the game (backmove foremove)
  */
 public class Log {
+	/**
+	 * Tree is an Advanced Log tools.
+	 * You can {@link Tree#goBackward(BoardGame, eColor)} in your move to try another move
+	 * And all is saved. Actually you can create your own Tree of Game !
+	 * 
+	 * each time we will talk about a move it wil represent
+	 * move: {@link Pair}<{@link Position}, {@link Position}>
+	 * @author Lumy-
+	 *
+	 */
 	private class Tree {
 		/**
 		 * is an Elem of the {@link Log.Tree}
@@ -40,21 +50,21 @@ public class Log {
 			 */
 			public String						StringAction;
 			/**
-			 * Represent The Played of the Node {@link Log.Tree.Elem} 
+			 * Represent The move of the Node {@link Log.Tree.Elem} 
 			 */
 			public Pair<Position, Position>		shoot;
 			/**
 			 * index is used in intern or in export.
 			 * Represent the move that is did in the Node {@link Log.Tree.Elem}
-			 * if -1 that mean no Played is made. (One End of {@link Log.Tree})
+			 * if -1 that mean no move is made. (One End of {@link Log.Tree})
 			 */
 			public int 							index;
 			/**
-			 * is different from null if the Played make a Eat pieces
+			 * is different from null if the move make a Eat pieces
 			 */
 			public ePawns						eaten;
 			/**
-			 * Store the current {@link eGameState}. in the Current Played.
+			 * Store the current {@link eGameState}. in the Current move.
 			 */
 			public eGameState					eState;
 			/**
@@ -97,58 +107,89 @@ public class Log {
 				eaten = eatn;
 			}
 		}
-
+		/**
+		 * Represent The current Log.Tree.Elem.
+		 * will be move like head = head.elems[index] || head = head.mother
+		 * {@link Log.Tree.Elem#elems}
+		 */
 		private Elem head;
+		/**
+		 * Name Player White
+		 */
 		public String WhiteName;
+		/**
+		 * Name Player Black
+		 */
 		public String BlackName;
+		/**
+		 * Result at PGN form @see <a href="http://en.wikipedia.org/wiki/Portable_Game_Notation">PGN</a>
+		 * 1-1 || 0-1 || 1/2-1/2 || "*"
+		 */
 		private String Result;
+		/**
+		 * Constructor. Create a new Head with null mother (the root) no move.(null)Result Undefined ("*") {@link #Result}
+		 * 
+		 */
 		public Tree() {
-			xstream = new XStream();
 			head = new Elem(null, eGameState.NEXT);
 			Result = "*";
 		}
 		/**
-		 * return the current Pair p
+		 * return the current move
 		 * @return
 		 */
 		public Pair<Position, Position>	getCurrentShoot() {
 			return head.shoot;
 		}
 		/**
-		 * return the size of head.Elem
+		 * return the size {@link Log.Tree.Elem#elems} of {@link Log.Tree#head}
 		 * @return
 		 */
 		public int getSizeElems() {
-			return head.index + 1;
+			return head.elems.size();
 		}
 		/**
-		 * Addthe Pair p and move on this Elem.
+		 * Add the move p and Forward the Log.
 		 * @param p
+		 * @param c
+		 * @param e
 		 */
 		public void addMoveHead(Pair<Position, Position> p, eGameState c, ePawns e) {
 			if (addCurrentHead(p, c, e)) // if False No Need to Move
 				goForwardElem();
 		}
 		/**
-		 * add +1 at head index and add p in head.elems
+		 * add 1 at {@link Log.Tree.Elem#index} of {@link Log.Tree#head} considerting that the default path and add p in head.elems
 		 * @param p
+		 * @param c
+		 * @param e
+		 * @return
 		 */
 		public boolean addCurrentHead(Pair<Position, Position> p, eGameState c, ePawns e) {
-			if (null != alreadyExist(p))
-				{
-					head = alreadyExist(p);
-					return false; //Already Move
-				}
+			if (null != alreadyExist(p)) {
+				head = alreadyExist(p); // Already Exist So Move on It.
+				return false; //Already Move
+			}
 			head.index += 1;
 			head.elems.add(new Elem(head, c, p, e));
 			return true;
 		}
+		/**
+		 * null if don't find p in {@link Log.Tree.Elem#elems} of {@link Log.Tree#head}
+		 * @param p
+		 * @return
+		 */
 		private Elem alreadyExist(Pair<Position, Position> p) {
 			for (Elem e : head.elems)
 				if (e.shoot != null && e.shoot.equals(p))
 					return e;
 			return null;
 		}
+		/**
+		 * null if don't find string in {@link Log.Tree.Elem#elems} of {@link Log.Tree#head}
+		 * @param string
+		 * @return
+		 */
 		private Elem alreadyExist(String string) {
 			for (Elem e : head.elems)
 				if (e.StringAction.equals(string))
@@ -156,7 +197,7 @@ public class Log {
 			return null;
 		}
 		/**
-		 * Add +1 at Head index and add String represent Castling (O-O || O-O-O)
+		 * Add +1 at Head index and add String represent {@link Rules.OptionalRules#Castling} (O-O || O-O-O)
 		 * Promotion use Position
 		 * @param string
 		 */
@@ -164,8 +205,9 @@ public class Log {
 			addString(string, null, c);
 		}
 		/**
-		 * Add +1 at Head index and add String represent Castling (O-O || O-O-O) OR Promotion
-		 * Castling p = null OR String = pos.print() + ePawns Or null
+		 * Add +1 at Head index and add String represent {@link Rules.OptionalRules#Castling} (O-O || O-O-O) OR {@link Rules.OptionalRules#Promotion}
+		 * Castling:[string="O-O[-O]"] OR Promotion:[string={@link ePawns}]
+		 * goToNextHead
 		 * @param string
 		 * @param p
 		 */
@@ -179,59 +221,69 @@ public class Log {
 			goForwardElem();
 		}
 		/**
-		 * Go on the current index
+		 * Go on the current {@link Log.Tree.Elem#index} of {@link Log.Tree#head}
 		 * @return
 		 */
 		public boolean goForwardElem() {
 			return goForwardElem(head.index);
 		}
 		/**
-		 * Go on one index beetween 0 and head.index
+		 * Go on one index beetween 0 and {@link Log.Tree.Elem.elems.size()} of {@link Log.Tree#head}
 		 * @param i
 		 * @return
 		 */
 		public boolean goForwardElem(int i) {
-			if (i >= 0 && i <= head.elems.size())
-			{
+			if (i >= 0 && i <= head.elems.size()){
 				head.index = i;
 				head = head.elems.get(i);
 				return true;
 			}
 			return false;
 		}
+		/**
+		 * Go Backward. Need {@link BoardGame} and current {@link eColor} player cause of
+		 * {@link BoardGame#UndoCastling(String, eColor)}
+		 * {@link BoardGame#UndoPromotion(Position)}
+		 * {@link BoardGame#undoRemove(ePawns, Position, eColor)}
+		 * @param elem
+		 * @param e
+		 * @return
+		 */
 		public boolean goBackward(BoardGame elem, eColor e) {
-			if (head.mother == null)
+			if (head.mother == null) // can't go backward
 				return false;
-			if (head.StringAction != null) {
-				//Action
-				if (t.head.StringAction.equals("O-O") || t.head.StringAction.equals("O-O-O")) {
+			if (head.StringAction != null) { // Special Action
+				if (t.head.StringAction.equals("O-O") || t.head.StringAction.equals("O-O-O")) { // Castling
 					elem.UndoCastling(t.head.StringAction, e);
 					head = head.mother;
 					return true;
 				}
-				else { // Promotion
+				else { // Promotion: Just Undo before UndoMove
 					elem.UndoPromotion(t.head.shoot.GetRight());
 				}
 			}
-			elem.get(elem.indexOf(head.shoot.GetRight())).SetPosition(head.shoot.GetLeft());
-			if (head.eaten != null) {
+			elem.get(elem.indexOf(head.shoot.GetRight())).SetPosition(head.shoot.GetLeft()); // Switch Position
+			if (head.eaten != null) { // Pawn has Eaten
 				Position n = new Position(head.shoot.GetRight());
-				if (elem.indexOf(elem.GetEaten(), n) == -1) {
+				if (elem.indexOf(elem.GetEaten(), n) == -1) { // EnPassantRules
 					n.row += 1;
-					if (elem.indexOf(elem.GetEaten(), n) == -1)
+					if (elem.indexOf(elem.GetEaten(), n) == -1) //EnPassantRules
 						n.row -= 2;
 				}
-				elem.undoRemove(head.eaten, n, (e == eColor.Black ? eColor.White : eColor.Black));
+				elem.undoRemove(head.eaten, n, (e == eColor.Black ? eColor.White : eColor.Black)); // UndoRemove
 			}
-			if (!isMouvement(head.shoot.GetLeft()))
-				elem.setInitPos(head.shoot.GetLeft());
-			head = head.mother;
+			if (!isMouvement(head.shoot.GetLeft())) // if the Pawn has NEVER move
+				elem.setInitPos(head.shoot.GetLeft()); // is can be reset to init pos
+			head = head.mother;// Backward
 			return true;
 		}
-
+		/**
+		 * return null if there is no Move in Forward
+		 * return Forward move from {@link Log.Tree#head} in {@link Log.Tree.Elem#elems}
+		 * @return
+		 */
 		public Collection<Pair<Position, Position>> getAllForwardShoot() {
-			if (head.index > -1)
-			{
+			if (head.index > -1) {
 				ArrayList<Pair<Position, Position>> ret = new ArrayList<Pair<Position,Position>>();
 				for (Elem e : head.elems)
 					ret.add(e.shoot);
@@ -239,17 +291,26 @@ public class Log {
 			}
 			return null;
 		}
+		/**
+		 * return true if the {@link Position} p match in any mother moves.
+		 * That means the Pawn in p is not in his initial pos. 
+		 * @param p
+		 * @return
+		 */
 		public boolean isMouvement(Position p) {
-			Elem i = (head.mother == null ? head : head.mother);
-			p.print();
-			while (i.mother != null)
-			{
+			Elem i = (head.mother == null ? head : head.mother); // do this to don't check the last elem if it's not the last.
+			while (i.mother != null) {
 				if (i.shoot != null && i.shoot.GetRight().equals(p))
 					return true;
 				i = i.mother;
 			}
 			return false;
 		}
+		/**
+		 * Use By {@link Log#Export(String)}
+		 * Use external library @see <a href="http://xstream.codehaus.org/">xStream</a>
+		 * @return
+		 */
 		public String toXml() {
             XStream xstream = new XStream();
             return xstream.toXML(this);
@@ -260,6 +321,7 @@ public class Log {
 	private XStream xstream;
 	
 	public Log(String nB, String nW) {
+		xstream = new XStream();
 		t = new Tree();
 		t.BlackName = nB;
 		t.WhiteName = nW;
